38273456775c5904b34e65bc074735547322ea51
Erfanmah7 <efn8mah@yahoo.com>
2024-10-18 19:39:50 +0330

    styles type

--- pages/about-us.js
+++ pages/about-us.js
@@ -1,5 +1,19 @@
+import styles from "../styles/about.module.scss";
+
 function About() {
-  return <div>About</div>;
+  return (
+    <div>
+      <h1 className={styles.h1}>About</h1>
+      <p>test</p>
+
+      <style jsx>{`
+        p {
+          background-color: red;
+          color: white;
+        }
+      `}</style>
+    </div>
+  );
 }
 
 export default About;
